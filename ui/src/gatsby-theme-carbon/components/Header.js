5302e3c2f47789de88abaea59e2d1c99e24424ca
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-06-02 17:34:38 -0500

    added the gatsby carbon template and along with the homepage

---
+++ ui/src/gatsby-theme-carbon/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from 'gatsby-theme-carbon/src/components/Header';
+
+const CustomHeader = props => (
+  <Header {...props}>
+    DevOps Garage Solution&nbsp;<span>Engineering</span>
+  </Header>
+);
+
+export default CustomHeader;
